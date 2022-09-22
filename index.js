090d5b990bb9ec3df149ba85cf134ba2098f5e17
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-09-22 12:14:44 +0200

    use socket io in the project

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
+//socket io require
+const socket = require('socket.io')
+
 const server = app.listen(4000, () => {
     console.log("listening to port 4000")
 })
 
 //static files
 app.use(express.static("public"))
+
+//socket io setup
+const io = socket(server)
+
+io.on("connection", (socket) => {
+    console.log("socket is connected", socket.id)
+}) 
