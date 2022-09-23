bbcbcc75c930995ca7f52f7756d2244f4639ff8c
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-09-23 16:19:35 +0200

    add brodcasting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = app.listen(4000, () => {
     console.log("listening to port 4000")
 })
 
-//static files
+//static files(all files in public)
 app.use(express.static("public"))
 
 //socket io setup
@@ -17,9 +17,13 @@ const io = socket(server)
 
 io.on("connection", (socket) => {
     console.log("socket is connected", socket.id)
-    
+    //handle chat event
     socket.on('chat', data => {
-        io.sockets.emit("chat",data)
+        io.sockets.emit("chat", data) //send data to front
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit("typing", data) //send data to front
     })
 }) 
 
--- public/chat.js
+++ public/chat.js
@@ -1,24 +1,41 @@
 //create connection
 const socket = io.connect('http://localhost:4000')
 
+
+//get elements needed from index.html
 output = document.getElementById("output");
 btn = document.getElementById("send");
 message = document.getElementById("message");
 handle = document.getElementById("handle");
+feedback = document.getElementById("feedback");
+
 
 
 
 //listen click event on the button
 btn.addEventListener("click", () => {
     //send data from the front to the server
-    socket.emit("chat", {
+    socket.emit("chat", { //name and elements that we want to send
         message: message.value,
         handle: handle.value,
 
     })
 })
 
-//listening news from the server
+message.addEventListener("keypress", () => {
+    //send data from the front to the server
+    socket.emit("typing", { //name and elements that we want to send
+        handle: handle.value,
+    })
+})
+
+//listening new event from the server chat
 socket.on('chat', (data) => {
+    feedback.innerHTML=""
     output.innerHTML += "<p><strong>" +data.handle+ ": </strong>" + data.message + "</p>"  
+})
+
+//listening new event from the server typing
+socket.on('typing', (data) => {
+    feedback.innerHTML = "<p><em>" +data.handle+" is typing<em></p>"  
 })
